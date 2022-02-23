11907be910354ee65b611be622189f72a80893ec
Sean Standen <SStanden@Adm2s-MBP.AppTechcorp.com>
2022-02-23 11:19:18 -0800

    Fixed a minor bug on Contact page

--- components/PageComponents/Contact/Map.js
+++ components/PageComponents/Contact/Map.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Map() {
   return (
     <div style={{ height: '450px', width: '100%', margin: 0, padding: 0 }}>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3341.5661661596105!2d-117.28875184939685!3d33.120487580775766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc73435c1975e7%3A0xf11f781bc091eb38!2sAppTech%20Payments%20Corp.!5e0!3m2!1sen!2sus!4v1645642798682!5m2!1sen!2sus" width="100%" height="450px" style={{ border: 0, margin: 0, padding: 0 }} allowfullscreen="true" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3341.5661661596105!2d-117.28875184939685!3d33.120487580775766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc73435c1975e7%3A0xf11f781bc091eb38!2sAppTech%20Payments%20Corp.!5e0!3m2!1sen!2sus!4v1645642798682!5m2!1sen!2sus" width="100%" height="450px" style={{ border: 0, margin: 0, padding: 0 }} allowFullScreen loading="lazy"></iframe>
     </div>
   )
 }
